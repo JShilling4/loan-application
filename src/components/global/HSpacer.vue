78ac5aa59efa1f54b1b181d280deb7fc9a5042dd
Justin Shilling <loveofmusic25@hotmail.com>
2021-08-01 01:06:05 -0400

    new layout components

---
+++ src/components/global/HSpacer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="horizontal-spacer" :style="styleObject"></div>
+</template>
+
+<script>
+    export default {
+        name: "HSpacer",
+        props: {
+            width: {
+                type: String,
+            }
+        },
+        data() {
+            return {
+                styleObject: {
+                    width: this.width,
+                }
+            };
+        },
+    }
+</script>
---
+++ src/components/global/VSpacer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="vertical-spacer" :style="styleObject"></div>
+</template>
+
+<script>
+    export default {
+        name: "VSpacer",
+        props: {
+            height: {
+                type: String,
+            }
+        },
+        data() {
+            return {
+                styleObject: {
+                    minHeight: this.height,
+                }
+            };
+        },
+    }
+</script>
